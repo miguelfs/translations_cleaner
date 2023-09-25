2a70dc76ceb174d350b55985e0f3ac74ba2f9e85
Miguel de Sousa <mfsousa@poli.ufrj.br>
2023-09-25 12:14:08 -0300

    Update delete_terms.dart

--- lib/src/delete_terms.dart
+++ lib/src/delete_terms.dart
@@ -34,5 +34,5 @@ Future<void> _deleteTermsForFile(
   }
   // Indent is being used for proper formatting
   await File(arbFile.path)
-      .writeAsString(JsonEncoder.withIndent(' ' * 1).convert(fileJson));
+      .writeAsString(JsonEncoder.withIndent(' ' * 2).convert(fileJson));
 }
